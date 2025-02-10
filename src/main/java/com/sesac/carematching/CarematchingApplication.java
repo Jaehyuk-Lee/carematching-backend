fe4eb52df403c7a5a48679549e6c2e7e41db85b2
Jaehyuk-Lee <alex00728@outlook.com>
2025-02-10 10:14:00 +0900

    initial commit

---
+++ src/main/java/com/sesac/carematching/CarematchingApplication.java
@@ -0,0 +1,13 @@
+package com.sesac.carematching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarematchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarematchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sesac/carematching/CarematchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesac.carematching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarematchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
