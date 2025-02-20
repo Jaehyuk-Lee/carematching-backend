4ca5bfb985ce07de99f4cc5d9af971016a615a66
Jaehyuk-Lee <alex00728@outlook.com>
2025-02-20 11:53:52 +0900

    Enable JPA auditing for entity classes

--- src/main/java/com/sesac/carematching/community/comment/Comment.java
+++ src/main/java/com/sesac/carematching/community/comment/Comment.java
@@ -11,12 +11,14 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
 @Getter
 @Setter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "community_comment")
 public class Comment {
     @Id
--- src/main/java/com/sesac/carematching/community/post/Post.java
+++ src/main/java/com/sesac/carematching/community/post/Post.java
@@ -12,12 +12,14 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
 @Getter
 @Setter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "community_post")
 public class Post {
     @Id
