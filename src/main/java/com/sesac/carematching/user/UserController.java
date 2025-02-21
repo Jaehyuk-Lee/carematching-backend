685abe3de0e3545565d4b08267696b99a88ba67b
Jaehyuk-Lee <alex00728@outlook.com>
2025-02-21 17:49:56 +0900

    Implement user registration and authentication

--- src/main/java/com/sesac/carematching/user/UserController.java
+++ src/main/java/com/sesac/carematching/user/UserController.java
@@ -1,18 +1,57 @@
 package com.sesac.carematching.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/users")
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
 
+    private final UserService userService;
+
     @PostMapping("/signup")
-    public ResponseEntity<Void> join(@RequestBody User user) {
+    public ResponseEntity<Void> join(@RequestBody UserSignupDTO user) {
         System.out.println("회원가입 컨트롤러 실행" + user);
-        userService.joinUser(user);
+        userService.registerUser(user);
         System.out.println("회원가입 완료");
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/loginOk")
+    public ResponseEntity<Map<String, String>> loginOk() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        String authorities = authentication.getAuthorities().toString();
+
+        Map<String, String> response = new HashMap<>();
+        response.put("email", username);
+        response.put("authorities", authorities);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/logoutOk")
+    public ResponseEntity<Void> logoutOk() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<User> getUserPage() {
+        System.out.println("일반 인증 성공");
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        // 유저 정보
+        User user = userService.getUserInfo(username);
+
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/sesac/carematching/user/UserService.java
+++ src/main/java/com/sesac/carematching/user/UserService.java
@@ -1,5 +1,74 @@
 package com.sesac.carematching.user;
 
+import com.sesac.carematching.user.role.RoleService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    private final RoleService roleService;
+
+    // 회원가입 로직
+    public void registerUser(UserSignupDTO dto) {
+
+        if(userRepository.findByUsername(dto.getUsername()).isPresent()){
+            throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
+        }
+
+        // 비밀번호 확인
+        if (!dto.getPassword().equals(dto.getConfirmPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // 아이디 중복 확인
+        if (!isUsernameAvailable(dto.getUsername())) {
+            throw new IllegalArgumentException("이미 사용 중인 아이디입니다.");
+        }
+
+        // 닉네임 중복 확인
+        if (!isNicknameAvailable(dto.getNickname())) {
+            throw new IllegalArgumentException("이미 있는 닉네임입니다.");
+        }
+
+        // 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(dto.getPassword());
+
+        // Users 객체 생성
+        User user = new User(
+            dto.getUsername(),
+            encodedPassword, // 암호화된 비밀번호 저장
+            dto.getNickname(),
+            roleService.findRoleByName("ROLE_USER")
+        );
+
+        // 사용자 저장
+        userRepository.save(user);
+    }
+
+    public User getUserInfo(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+
+    // 아이디 중복 확인
+    public boolean isUsernameAvailable(String username) {
+        return userRepository.findByUsername(username).isEmpty();
+    }
+
+    // 닉네임 중복 확인
+    public boolean isNicknameAvailable(String nickname) {
+        return userRepository.findByNickname(nickname).isEmpty();
+    }
+
+    // 회원 탈퇴
+    @Transactional
+    public void deleteUser(String username){
+        userRepository.deleteByUsername(username);
+    }
 }
