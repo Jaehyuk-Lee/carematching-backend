e7ac9ba55d813462b6597f300bb51865c4559c6a
jeeyun2 <irisjeeyun@naver.com>
2025-02-24 15:25:47 +0900

    UserRepository 빠진거 추가

--- src/main/java/com/sesac/carematching/user/UserRepository.java
+++ src/main/java/com/sesac/carematching/user/UserRepository.java
@@ -2,5 +2,10 @@ package com.sesac.carematching.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByNickname(String nickname);
+    void deleteByUsername(String username);
 }
