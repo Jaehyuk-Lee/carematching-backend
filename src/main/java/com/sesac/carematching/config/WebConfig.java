83190092c61590bca7155708d42a288df6471fbb
Jaehyuk-Lee <alex00728@outlook.com>
2025-02-28 16:29:12 +0900

    add domain names on CORS settings

--- src/main/java/com/sesac/carematching/config/WebConfig.java
+++ src/main/java/com/sesac/carematching/config/WebConfig.java
@@ -8,17 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // 모든 API 엔드포인트에 대해 CORS 설정
-                        .allowedOrigins("http://localhost:3000") // 허용할 출처
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
-                        .allowedHeaders("*") // 허용할 헤더
-                        .allowCredentials(true); // 자격 증명 허용
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // 모든 API 엔드포인트에 대해 CORS 설정
+        .allowedOrigins("https://d12hp6zm8su88f.cloudfront.net/",
+                    "https://carematching.net/", "https://www.carematching.net/",
+                    "http://localhost:3000", "http://localhost") // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true); // 자격 증명 허용
     }
 }
