24fd0c0667d883c8cfa422d3fbf1d48bf3e3185f
이지윤 <irisjeeyun@naver.com>
2025-03-07 17:35:33 +0900

    Review (#41)
    * delete exercise
    
    * review + private 변경

--- src/main/java/com/sesac/carematching/caregiver/experience/ExperienceService.java
+++ src/main/java/com/sesac/carematching/caregiver/experience/ExperienceService.java
@@ -33,7 +33,7 @@ public class ExperienceService {
         return experienceRepository.findByCaregiver(caregiver);
     }
 
-    public Experience toEntity(ExperienceRequest dto, Caregiver caregiver) {
+    private Experience toEntity(ExperienceRequest dto, Caregiver caregiver) {
         return Experience.builder()
             .title(dto.getTitle())
             .summary(dto.getSummary())
