c89acd77347522695fa5153720c7654b423db641
alljju <cyjj625@gmail.com>
2025-03-08 20:31:10 +0900

    main merge

--- src/main/java/com/sesac/carematching/chat/service/RoomService.java
+++ src/main/java/com/sesac/carematching/chat/service/RoomService.java
@@ -9,6 +9,8 @@ public interface RoomService {
 
     RoomResponse getRoom(Integer roomId);
 
+    List<RoomResponse> getUserRooms(Integer id);
+
     @Transactional(readOnly = true)
     List<RoomResponse> getUserRooms(String username);
 
