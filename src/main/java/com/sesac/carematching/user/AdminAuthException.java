e816612b2220ca580e5e20bf4d41a1ac6fe2ce78
Jaehyuk-Lee <alex00728@outlook.com>
2025-03-09 17:44:19 +0900

    Refactor Token related error (401) and add Exception Handler for AdminPrivileges (403)

---
+++ src/main/java/com/sesac/carematching/user/AdminAuthException.java
@@ -0,0 +1,7 @@
+package com.sesac.carematching.user;
+
+public class AdminAuthException extends RuntimeException {
+    public AdminAuthException(String message) {
+        super(message);
+    }
+}
