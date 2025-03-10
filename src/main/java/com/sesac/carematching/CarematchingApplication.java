c76491054b23250405dc399e350192ec9b8adac7
Lee Jaehyuk <alex00728@outlook.com>
2025-03-10 22:28:22 +0900

    update Timezone to UTC+9 (#63)

--- src/main/java/com/sesac/carematching/CarematchingApplication.java
+++ src/main/java/com/sesac/carematching/CarematchingApplication.java
@@ -1,11 +1,12 @@
 package com.sesac.carematching;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -15,4 +16,10 @@ public class CarematchingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarematchingApplication.class, args);
 	}
+
+    @PostConstruct
+    public void init() {
+        // timezone 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+9:00"));
+    }
 }
