c82e645d81c12357aaf511a416d2a10d60275038
Jaehyuk-Lee <alex00728@outlook.com>
2025-03-10 09:22:46 +0900

    use environment variable

--- src/main/java/com/sesac/carematching/chat/config/RedisConfig.java
+++ src/main/java/com/sesac/carematching/chat/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.sesac.carematching.chat.config;
 
 import com.sesac.carematching.chat.service.NotificationService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,9 +14,15 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 @Configuration
 public class RedisConfig {
 
+    @Value("${redis.host}")
+    private String redisHost;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("127.0.0.1", 6379); // Redis 서버와 연결
+        return new LettuceConnectionFactory(redisHost, redisPort); // 환경변수를 통한 Redis 서버 연결
     }
 
     @Bean
