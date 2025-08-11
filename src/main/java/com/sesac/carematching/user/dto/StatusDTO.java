666cb5b900f8eb807eba2d1a34a8c04eeb246455
Jaehyuk-Lee <alex00728@outlook.com>
2025-08-11 18:15:55 +0900

    feat: Swagger 주석 추가로 DTO 문서화 개선

--- src/main/java/com/sesac/carematching/user/dto/StatusDTO.java
+++ src/main/java/com/sesac/carematching/user/dto/StatusDTO.java
@@ -1,8 +1,11 @@
 package com.sesac.carematching.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+@Schema(description = "상태 DTO")
 @Data
 public class StatusDTO {
+    @Schema(description = "상태", allowableValues = {"true", "false"})
     private boolean status;
 }
