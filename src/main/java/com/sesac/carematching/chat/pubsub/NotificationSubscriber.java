35f625e4f91d7b47ad5f281335eb5c07adbe1d3c
Jaehyuk-Lee <alex00728@outlook.com>
2025-08-13 15:57:57 +0900

    refactor: 알림 기능 Redis Pub/Sub 리팩토링 - NotificationService에서 RedisPublisherService 사용 - RedisSubscriber에서 NotificationSubscriber로 이름 변경

--- src/main/java/com/sesac/carematching/chat/pubsub/NotificationSubscriber.java
+++ src/main/java/com/sesac/carematching/chat/pubsub/NotificationSubscriber.java
@@ -1,4 +1,4 @@
-package com.sesac.carematching.chat.listener;
+package com.sesac.carematching.chat.pubsub;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class RedisSubscriber {
+public class NotificationSubscriber {
 
     private final SimpMessagingTemplate messagingTemplate;
 
