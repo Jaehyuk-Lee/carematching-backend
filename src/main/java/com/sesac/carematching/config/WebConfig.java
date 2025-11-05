9033d91f217aabe051f5486e76924d75e5b1a924
Lee Jaehyuk <alex00728@outlook.com>
2025-11-05 22:06:12 +0900

    Revert "feat: Gateway에서 CORS 제어 및 기존 CORS 설정 제거 (#99)" (#100)
    This reverts commit 862f126e8d2ce360c697fa6c2c3eb0b82982cee8.

--- src/main/java/com/sesac/carematching/config/WebConfig.java
+++ src/main/java/com/sesac/carematching/config/WebConfig.java
@@ -13,4 +13,15 @@ public class WebConfig implements WebMvcConfigurer, WebMvcRegistrations {
     public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
         return new ApiVersionRMHM("/api");
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // 모든 API 엔드포인트에 대해 CORS 설정
+        .allowedOrigins("https://d12hp6zm8su88f.cloudfront.net/",
+                    "https://carematching.net/", "https://www.carematching.net/",
+                    "http://localhost:3000", "http://localhost") // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true); // 자격 증명 허용
+    }
 }
