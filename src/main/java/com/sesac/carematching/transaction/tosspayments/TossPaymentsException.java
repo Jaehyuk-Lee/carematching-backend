c615c8c6116f1c71b8186168c5f0a59025b7f811
Jaehyuk-Lee <alex00728@outlook.com>
2025-11-06 14:57:48 +0900

    chores: 토스페이먼츠 API 에러 코드 문서 링크 추가

--- src/main/java/com/sesac/carematching/transaction/tosspayments/TossPaymentsException.java
+++ src/main/java/com/sesac/carematching/transaction/tosspayments/TossPaymentsException.java
@@ -1,5 +1,6 @@
 package com.sesac.carematching.transaction.tosspayments;
 
+// 토스페이먼츠 API 에러 코드 문서: https://docs.tosspayments.com/reference/error-codes#%EA%B2%B0%EC%A0%9C-%EC%8A%B9%EC%9D%B8
 public class TossPaymentsException extends RuntimeException {
     private final String code;
     private final String message;
