ccc3c1c8c08dc27757b5f4f1b905e7cb44ef3938
Jaehyuk-Lee <alex00728@outlook.com>
2025-11-08 23:32:18 +0900

    feat: KakaoPay Approve body에 partner_user_id 추가

--- src/main/java/com/sesac/carematching/transaction/payment/provider/kakao/KakaoPayService.java
+++ src/main/java/com/sesac/carematching/transaction/payment/provider/kakao/KakaoPayService.java
@@ -60,6 +60,7 @@ public class KakaoPayService extends AbstractPaymentService {
         requestData.put("partner_order_id", request.getOrderId());
         requestData.put("total_amount", request.getAmount());
         requestData.put("tid", request.getPaymentKey());
+        requestData.put("partner_user_id", request.getPartnerUserId());
         requestData.put("pg_Token", request.getPgToken());
 
         HttpHeaders headers = new HttpHeaders();
