9dbbff7433ad97b43bbcd935b7e42ee67badcc87
Jaehyuk-Lee <alex00728@outlook.com>
2025-11-09 03:11:22 +0900

    feat: PendingPaymentRepository 제거 (불필요한 인터페이스 삭제)

--- src/main/java/com/sesac/carematching/transaction/payment/pendingPayment/PendingPaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sesac.carematching.transaction.payment.pendingPayment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PendingPaymentRepository extends JpaRepository<PendingPayment, Integer> {
-
-}
